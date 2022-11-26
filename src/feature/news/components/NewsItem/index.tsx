92d6eb2db62d794361a509189c9995836cdbab7c
oleg <music.wave.form7@gmail.com>
2022-11-26 18:20:56 +0100

    (feature): some  basic styling

--- src/feature/news/components/NewsItem/index.tsx
+++ src/feature/news/components/NewsItem/index.tsx
@@ -9,13 +9,15 @@ export default function NewsItem(props: NewsProps){
     const { newsItem } = props;
     return (
      <div className={styles.container} key={newsItem.id}>
-        <div><span>{'id: '}</span>{newsItem.id}</div>
-        <div><span>{'title: '}</span>{newsItem.title}</div>
-        <div><span>{'url: '}</span>{newsItem.url}</div>
-        <div><span>{'score: '}</span>{newsItem.score}</div>
-        <div><span>{'time: '}</span>{newsItem.time}</div>
-        <div><span>{'karma: '}</span>{newsItem.karma}</div>
-        <img src={newsItem.img}/>
+        <div>
+            <div><span>{'id: '}</span>{newsItem.id}</div>
+            <div><span>{'title: '}</span>{newsItem.title}</div>
+            <div><span>{'url: '}</span>{newsItem.url}</div>
+            <div><span>{'score: '}</span>{newsItem.score}</div>
+            <div><span>{'time: '}</span>{newsItem.time}</div>
+            <div><span>{'karma: '}</span>{newsItem.karma}</div>
+        </div>
+        <img className={styles.image} src={newsItem.img}/>
     </div>
    )
 }
